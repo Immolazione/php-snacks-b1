f8bde014bdcb3586c90ac897f91a93bd73868ec2
Immolazione <federico.deered@gmail.com>
2022-03-01 19:29:28 +0100

    add snack2

--- snack2.php
+++ snack2.php
@@ -6,4 +6,74 @@ Creare un array di array. Ogni array figlio avrà come chiave una data in questo
 Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z
 */
 
+$posts_by_date = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
+$dates = array_keys($posts_by_date);
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php for($i = 0; $i < count($posts_by_date); $i++) {
+        $date = $dates[$i];
+        $posts = $posts_by_date[$date];
+    ?>
+
+    <div>
+        <h3><?= $date ?></h3>
+        <?php for($k = 0; $k < count($posts); $k++) {
+            $post = $posts[$k];
+        ?>
+        <h4><?= $post['title'] ?></h4>
+        <p><?= $post['text'] ?></p>
+        <p><?= $post['author'] ?></p>
+
+        <?php } ?>
+    </div>
+    <?php } ?>
+</body>
+</html>
